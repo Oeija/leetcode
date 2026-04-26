432d2dd5275f67d16543341a5c72d5600c86b615
vincentoei <oei.vincent20@gmail.com>
2026-04-26 22:43:07 +0700

    fix: fixed solution arrays.41

--- arrays/41_first-missing-positive.py
+++ arrays/41_first-missing-positive.py
@@ -2,35 +2,15 @@ class Solution(object):
     def firstMissingPositive(self, nums):
 
         n = len(nums)
-        int1 = 1
-        int2 = 0
 
+        for x in range(n):
+            while 1 <= nums[x] <= n and nums[nums[x] - 1] != nums[x]:
+                # swap nums[i] into its correct position
+                correct_idx = nums[x] - 1
+                nums[x], nums[correct_idx] = nums[correct_idx], nums[x]
 
-        for i, x in enumerate(nums):
-            if i == 0 and nums[i] > 0:
-                int1 = x
-                if n == 1:
-                    if int1 > 1 or int1 <= 0:
-                        return 1
-                    else:
-                        return int1+1
-                    
-            if x > 0 and x <= int1:
-                int1 = x
+        for x in range(n):
+            if nums[x] != x + 1:
+                return x + 1
 
-            if int1 <= 0 and i == n-1:
-                return 1
-        
-        print(int1)
-
-
-        if int1 > 1:
-            return 1
-
-
-        for x in range(1, n+1):
-            if x not in nums:
-                return x
-        
-        if int1 == 1:
-            return n+1
+        return n + 1
