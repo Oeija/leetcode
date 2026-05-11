0e329e83299852ae147a9143d586afd695ac7cb7
vincentoei <oei.vincent20@gmail.com>
2026-05-11 23:49:43 +0700

    solved: bit-manipulation.191_number-of-1-bits

---
+++ bit-manipulation/191_number-of-1-bits.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hammingWeight(self, n):
+
+        count = 0
+
+        while n:
+            n = n & (n - 1)
+            count += 1
+
+        return count
+        
+        """
+        :type n: int
+        :rtype: int
+        """
+        
